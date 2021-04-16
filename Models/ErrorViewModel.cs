57208878534704188bd3c6012403fccca9024c5a
TuanKhai2512 <trankhai2512.ttk@gmail.com>
2021-04-16 16:46:38 +0700

    front end
    Finished front end

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BaiLam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
